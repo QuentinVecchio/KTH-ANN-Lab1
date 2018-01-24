ea4195e598f7a69c69c1eabfa40eaa18c3311562
Yeramian Kevin <kevin.yeramian@eyesnap.fr>
2018-01-24 22:33:19 +0100

    Test of SingleLayerNN : ok Test of Perceptron: fail

--- scr/NN.py
+++ scr/NN.py
@@ -8,16 +8,30 @@ class SingleLayerNN():
         self.nb_eboch = nb_eboch
         self.W = []
 
-    def fit(self, X, Y):  # X = (len(X[0]) + 1, n)
-        X = np.vstatck([X, [1]*len(X[0])])
-        W = np.reshape(np.random.normal(0, 1, len(X[0])), (1, len(X[0])))
-        for step in range(self.nb_eboch):
-            WX = np.multiply(W, X)  # Prediction : (1, len(X[0]) + 1) * (len(X[0]) + 1, n) =(1, n)
-            diff = self.lr * np.multiply(WX - Y, X.T)
-            W = W + diff
+    def posneg(self, WX):
+        output = []
+        for x in WX.T:
+            if (x < 0):
+                output.append(-1)
+            else:
+                output.append(1)
 
-    def train(self, X):
-        return np.multiply(W, X)[:-1]
+        return output
+
+    def fit(self, X, Y):  # X = (len(X[0]) + 1, n)
+        X = np.vstack([X, [1] * len(X[0])])
+        self.W = np.reshape(np.random.normal(0, 1, len(X)), (1, len(X)))
+        for step in range(self.nb_eboch):
+            WX = np.matmul(self.W, X)  # Prediction : (1, len(X)) * (len(X), n) =(1, n)
+            WX = self.posneg(WX)
+            aux = WX - Y
+            diff = self.lr * np.matmul(aux, X.T)
+            self.W = self.W - diff
+
+    def predict(self, X):
+        X = np.vstack([X, [1] * len(X[0])])
+        return self.posneg(np.matmul(self.W, X))
+
 
 class Perceptron():
     def __init__(self, lr=0.1, nb_eboch=20):
@@ -25,13 +39,23 @@ class Perceptron():
         self.nb_eboch = nb_eboch
         self.W = []
 
-    def fit(self, X, Y):  # X = (len(X[0]) + 1, n)
-        X = np.vstatck([X, [1]*len(X[0])])
-        W = np.reshape(np.random.normal(0, 1, len(X[0])), (1, len(X[0])))
-        for step in range(self.nb_eboch):
-            WX = np.multiply(W, X)  # Prediction : (1, len(X[0]) + 1) * (len(X[0]) + 1, n) =(1, n)
-            diff = self.lr * (Y - WX) * X
-            W = W + diff
+    def posneg(self, WX):
+        output = []
+        for x in WX.T:
+            if (x < 0):
+                output.append(-1)
+            else:
+                output.append(1)
+        return output
 
-    def train(self, X):
-        return np.multiply(W, X)[:-1]
+    def fit(self, X, Y):  # X = (len(X[0]) + 1, n)
+        X = np.vstack([X, [1] * len(X[0])])
+        self.W = np.reshape(np.random.normal(0, 1, len(X[0])), (1, len(X[0])))
+        for step in range(self.nb_eboch):
+            WX = self.posneg(np.matmul(self.W, X))  # Prediction : (1, len(X[0]) + 1) * (len(X[0]) + 1, n) =(1, n)
+            diff = self.lr * (Y - WX) * X
+            self.W = self.W + diff
+
+    def predict(self, X):
+        X = np.vstack([X, [1] * len(X[0])])
+        return np.matmul(self.W, X)
--- scr/main.py
+++ scr/main.py
@@ -1,25 +1,35 @@
-from NN import NN1
+from NN import SingleLayerNN
+from NN import Perceptron
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def generateDataSet():
-    X = list(np.random.multivariate_normal([-3, -3], [[1, 0], [0, 1]], 1000))
-    X += list(np.random.multivariate_normal([3, 3], [[1, 0], [0, 1]], 1000))
-    Y = [1] * 1000 + [-1] * 1000
+    X = list(np.random.multivariate_normal([-3, -3], [[1, 0], [0, 1]], 100))
+    X += list(np.random.multivariate_normal([3, 3], [[1, 0], [0, 1]], 100))
+    Y = [1] * 100 + [-1] * 100
     p = np.random.permutation(len(X))
     return np.array(X)[p], np.array(Y)[p]
-def showData2D(X,Y):
+
+
+def showData2D(X, Y):
     fig = plt.figure(figsize=(10, 10))
     colors = ['red', 'blue']
     plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=matplotlib.colors.ListedColormap(colors))
     plt.show()
 
+
 def test():
     # nn = NN1()
     X, Y = generateDataSet()
-    showData2D(X,Y)
+    modelSingleLayer = Perceptron()
+    modelSingleLayer.fit(X.T, Y.T)
+    X1, Y2 = generateDataSet()
+    print(Y2)
+    print(modelSingleLayer.predict(X1.T) - Y2)
+
+
 
 if __name__ == '__main__':
     test()
