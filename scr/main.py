031474029a499b09ed83b03d6940291f7d2720c7
Kevin <kevin.yeramian@telecom-paristech.fr>
2018-01-24 19:02:57 +0100

    Feat : Add a function in main that generate 2D gaussian separable to test our model. (#10)
    Closes #9

--- scr/main.py
+++ scr/main.py
@@ -1,5 +1,14 @@
 from NN import NN1
+import numpy as np
+def generateDataSet():
+    X = list(np.random.multivariate_normal([-3, -3], [[1, 0], [0, 1]], 1000))
+    X += list(np.random.multivariate_normal([3, 3], [[1, 0], [0, 1]], 1000))
+    Y = [1] * 1000 + [-1] * 1000
+    p = np.random.permutation(len(X))
+    return np.array(X)[p], np.array(Y)[p]
 def test():
-    nn = NN1()
+    #nn = NN1()
+    X, Y = generateDataSet()
+    print(Y)
 if __name__ == '__main__':
     test()
