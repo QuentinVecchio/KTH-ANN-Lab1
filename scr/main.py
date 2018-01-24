c408df8dcaa9c983b70bd45431eda56516a839b4
Yeramian Kevin <kevin.yeramian@eyesnap.fr>
2018-01-24 19:22:57 +0100

    Add a function that show 2D data.

--- scr/main.py
+++ scr/main.py
@@ -1,14 +1,25 @@
 from NN import NN1
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+
+
 def generateDataSet():
     X = list(np.random.multivariate_normal([-3, -3], [[1, 0], [0, 1]], 1000))
     X += list(np.random.multivariate_normal([3, 3], [[1, 0], [0, 1]], 1000))
     Y = [1] * 1000 + [-1] * 1000
     p = np.random.permutation(len(X))
     return np.array(X)[p], np.array(Y)[p]
+def showData2D(X,Y):
+    fig = plt.figure(figsize=(10, 10))
+    colors = ['red', 'blue']
+    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.show()
+
 def test():
-    #nn = NN1()
+    # nn = NN1()
     X, Y = generateDataSet()
-    print(Y)
+    showData2D(X,Y)
+
 if __name__ == '__main__':
     test()
