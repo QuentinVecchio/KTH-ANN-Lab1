35694b1275e9fb1c4ef7e456c8d46872e47649d5
QuentinLemaire <lemairequentin22@gmail.com>
2018-01-29 15:05:08 +0100

    Implement test and results

--- src/NN_part2.py
+++ src/NN_part2.py
@@ -3,11 +3,11 @@ from sklearn.neural_network import MLPRegressor
 import numpy as np
 
 class NN():
-    def __init__(self, name, lr = 0.01, hidden_states = (5,), max_ite = 1000, momentum = 0.9):
+    def __init__(self, name, lr = 0.01, hidden_states = (5,), max_ite = 1000, momentum = 0.9, alpha = 0.01, early_stopping = "false", learning_rate = 'invscaling'):
         self.name = name
         self.prediction = []
         self.clf = MLPRegressor(solver='sgd', learning_rate_init=lr,
-                     hidden_layer_sizes=hidden_states, max_iter=max_ite, momentum=momentum, alpha=0.01, early_stopping=False, learning_rate='invscaling')
+                     hidden_layer_sizes=hidden_states, max_iter=max_ite, momentum=momentum, alpha=alpha, early_stopping=early_stopping, learning_rate=learning_rate)
 
     def fit(self, X, Y):
         self.clf.fit(X, Y)
@@ -24,3 +24,6 @@ class NN():
 
     def getMSE(self):
         return self.MSE
+
+    def getPrediction(self):
+        return self.prediction
