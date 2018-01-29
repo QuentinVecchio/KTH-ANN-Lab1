bfafe7c6fcbf0ca2e93c98a0cbb125f1836b0ba4
Yeramian Kevin <kevin.yeramian@eyesnap.fr>
2018-01-29 11:53:29 +0100

    Add option of regularization

--- src/NN_part2.py
+++ src/NN_part2.py
@@ -1,9 +1,9 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.neural_network import MLPRegressor
 class NN():
-    def __init__(self, lr = 0.001, hidden_states = (5, ), max_ite = 2000, momentum = 0.9):
-        self.clf = MLPRegressor(solver='lbfgs', learning_rate_init=lr,
-                     hidden_layer_sizes=hidden_states, max_iter=max_ite, momentum=momentum)
+    def __init__(self, lr = 0.01, hidden_states = (5,), max_ite = 1000, momentum = 0.9):
+        self.clf = MLPRegressor(solver='sgd', learning_rate_init=lr,
+                     hidden_layer_sizes=hidden_states, max_iter=max_ite, momentum=momentum, alpha=0.01, early_stopping=False, learning_rate='invscaling')
     def fit(self, X, Y):
         self.clf.fit(X, Y)
 
