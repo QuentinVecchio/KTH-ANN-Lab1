7e03a83bca02d7fc1053af7ee0c59318bed83030
QuentinVecchio <vecchioquentin@hotmail.fr>
2018-01-29 16:24:27 +0100

    Fix: Multiple Layer NN doesn't work #20 (#21)
    * Add animation
    
    * Update graph
    
    * Fix bug phy
    
    * Update

--- src/graph.py
+++ src/graph.py
@@ -74,39 +74,50 @@ def plotDecisionBoundaryAnim(title, X, T, WHistory):
     fig, ax = plt.subplots()
 
     colors = ['red', 'blue']
+    colorsNeuron = ['green', 'blue']
     ax.scatter(X[:, 0], X[:, 1], c=[colors[i] for i in T])
 
     x = 1
     y = 0
     bias = 2
+    lines = []
 
     ymin, ymax = plt.ylim()
-    w = [WHistory[0].item(x), WHistory[0].item(y)]
-    a = -w[x] / w[y]
-    xx = np.linspace(ymin, ymax)
-    yy = a * xx - (WHistory[0].item(bias)) / w[y]
-    line, = ax.plot(yy, xx, 'k-')
 
-    ttl = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+    for index, W in enumerate(WHistory[0]):
+        print(index)
+        w = [W.item(x), W.item(y)]
+        a = -w[x] / w[y]
+        xx = np.linspace(ymin, ymax)
+        yy = a * xx - (W.item(bias)) / w[y]
+        line, = ax.plot(yy, xx)
+        lines.append(line)
+
+    lines.append(ax.text(0.05, 0.9, '', transform=ax.transAxes))
 
     def animate(i):
-        ttl.set_text('Decision Boundary Iteration ' + str(i % len(WHistory)))
-        w = [WHistory[i].item(x), WHistory[i].item(y)]
-        a = -w[x] / w[y]
-        yy = a * xx - (WHistory[i].item(bias)) / w[y]
-        line.set_xdata(yy)  # update the data
-        return ttl, line
+        lines[-1].set_text('Decision Boundary Iteration ' + str(i % len(WHistory)))
+        for index, W in enumerate(WHistory[i]):
+            w = [W.item(x), W.item(y)]
+            a = -w[x] / w[y]
+            yy = a * xx - (W.item(bias)) / w[y]
+            lines[index].set_xdata(yy)
+
+        return tuple(lines)
+
 
     def init():
-        ttl.set_text('Decision Boundary Iteration ' + str(0))
-        w = [WHistory[0].item(x), WHistory[0].item(y)]
-        a = -w[x] / w[y]
-        yy = a * xx - (WHistory[0].item(bias)) / w[y]
-        line.set_xdata(yy)
-        return ttl, line,
+        lines[-1].set_text('Decision Boundary Iteration ' + str(0))
+        for index, W in enumerate(WHistory[0]):
+            w = [W.item(x), W.item(y)]
+            a = -w[x] / w[y]
+            yy = a * xx - (W.item(bias)) / w[y]
+            lines[index].set_xdata(yy)
+
+        return tuple(lines)
 
     ani = animation.FuncAnimation(
-        fig, animate, init_func=init, frames=len(WHistory), blit=True)
+        fig, animate, init_func=init, frames=len(WHistory), blit=True, interval=25)
 
     plt.title(title)
     plt.show()
--- src/scenarios.py
+++ src/scenarios.py
@@ -35,7 +35,7 @@ def scenario3_1_3():
     graph.plotNNInformations("Perceptron", X,T,  WHistoryPer[-1], eHistoryPer)
 
 def scenario3_2_1():
-    N = 500
+    N = 100
     V = 5
     X = list(np.random.multivariate_normal([V, V], [[1, 0], [0, 1]], N)) # Blue
     X += list(np.random.multivariate_normal([-V, -V], [[1, 0], [0, 1]], N)) # Blue
@@ -44,9 +44,22 @@ def scenario3_2_1():
     T = [1] * 2*N + [-1] * 2*N
     p = np.random.permutation(len(X))
     X, T = (np.array(X)[p]).T, np.array(T)[p]
-    multipleLayerNN = NN2.SingleLayerNN2(batch_size=-1)
+    multipleLayerNN = NN2.MultipleLayer(batch_size=-1, nb_eboch=1000, lr=0.01, hidden_layer_size=2)
     WHistoryMNN, eHistoryMNN = multipleLayerNN.fit(X, T)
     graph.plotNNInformations("Multiple Layer NN", X, T, WHistoryMNN[-1], eHistoryMNN)
+    graph.plotDecisionBoundaryAnim("Multiple Layer NN Anim", X, T, WHistoryMNN)
+
+def scenario3_2_2():
+    N = 8
+    H = 3
+    X = np.ones((N, N)) * -1
+    indices_diagonal = np.diag_indices(N)
+    X[indices_diagonal] = 1
+    print(X)
+    multipleLayerNN = NN2.MultipleLayer(hidden_layer_size=H, batch_size=-1)
+    WHistory, eHistory = multipleLayerNN.fit(X, X)
+    graph.plotError("Learning Rate Encoder", eHistory)
+    #graph.plotDecisionBoundaryAnim("Multiple Layer NN Anim", X, T, WHistoryMNN)
 
 def test():
     X, T = generateDataSet()
